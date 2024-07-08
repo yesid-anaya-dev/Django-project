f7c7a6faa17a593a74eedc534b5cb62a29d77ef3
Yesid Anaya <oyesidanaya@gmail.com>
2024-07-08 15:16:06 -0500

    Edit tasks and complete/pending tasks

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -32,6 +32,36 @@ def delete_task(request, task_id):
     return redirect('todolist')
 
 
+def edit_task(request, task_id):
+    task_obj = TaskList.objects.get(pk=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST or None, instance= task_obj)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Task Updated!!"))
+        return redirect('todolist')
+    else:
+        context = {
+            'edit_text': 'Editing the Task...',
+            'task_obj': task_obj
+            }
+        return render(request, 'edit.html', context)
+
+
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect('todolist')
+
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
+
+
 def about(request):
     context = {'about_text': 'Welcome to About page.'}
     return render(request, 'about.html', context)
